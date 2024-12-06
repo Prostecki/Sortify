40b34df30d7668846905f04b57ff6f112a435295
Maxx2774 <maxviggonordin@hotmail.se>
2024-12-06 20:14:58 +0100

    page, form, nav

---
+++ src/layout/Nav.jsx
@@ -0,0 +1,10 @@
+import { Link } from "react-router-dom";
+export default function Nav() {
+  return (
+    <>
+      <Link to="/">Home</Link>
+      <Link to="/">Log in</Link>
+      <Link to="/">Register</Link>
+    </>
+  );
+}
