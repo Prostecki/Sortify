0eae919aa2c575319f3c2636ac17f4db9d524f3a
Maxx2774 <maxviggonordin@hotmail.se>
2024-12-06 20:28:16 +0100

    home, login, profile, register page, form

--- src/layout/Nav.jsx
+++ src/layout/Nav.jsx
@@ -2,9 +2,12 @@ import { Link } from "react-router-dom";
 export default function Nav() {
   return (
     <>
-      <Link to="/">Home</Link>
-      <Link to="/">Log in</Link>
-      <Link to="/">Register</Link>
+      <nav className="flex text-[40px] gap-[20px] justify-center">
+        <Link to="/">Home</Link>
+        <Link to="/login">Log in</Link>
+        <Link to="/register">Register</Link>
+        <Link to="/profile">Profile</Link>
+      </nav>
     </>
   );
 }
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,5 +1,10 @@
-export default function HomePage(){
-    return(
-        
-    )
+import Nav from "../layout/Nav";
+
+export default function HomePage() {
+  return (
+    <>
+      <Nav />
+      <h1>Home Page</h1>
+    </>
+  );
 }
