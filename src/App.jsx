637a1319c48bf96bab26430049402ce973cf2601
Mark Taratynov <mark.taratynov@gmail.com>
2024-12-06 20:13:19 +0100

    Created Routes for pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,17 @@
 import { useState } from "react";
+import { Routes, Route } from "react-router-dom";
 import "./App.css";
 
 function App() {
   return (
     <>
       <h1>HEJ</h1>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="'/login" element={<LoginPage />} />
+        <Route path="'/profile" element={<ProfilePage />} />
+        <Route />
+      </Routes>
     </>
   );
 }
