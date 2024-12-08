94a16d9131b3e4c27af4ab445b0c481c8d18f39d
hassohamid <hassohamid@hotmail.com>
2024-12-08 03:59:51 +0100

    Simplified future use of page. Added instructrions for assignee to work on.

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -4,7 +4,17 @@ export default function HomePage() {
   return (
     <>
       <Nav />
-      <h1>Home Page</h1>
+      <h1 className="text-center text-[3rem]"> Dashboard/Home </h1>
+      <h1 className="text-center text-[2rem]">
+        User sees this page upon authentication (Logged in)
+      </h1>
+      <h1 className="text-center text-[3rem]"> Greet user. Hi (User) </h1>
+      <h1 className="text-center text-[3rem]">
+        Random Quote via API everytime user enters the Dashboard
+      </h1>
+      <h1 className="text-center text-[3rem]">
+        Overview showing user activity. Minimalistic.
+      </h1>
     </>
   );
 }
--- src/pages/ProfilePage.jsx
+++ src/pages/ProfilePage.jsx
@@ -4,7 +4,10 @@ export default function ProfilePage() {
   return (
     <>
       <Nav />
-      <h1>Profile Page</h1>
+      <h1 className="text-center text-[3rem]"> Profile </h1>
+      <h1 className="text-center text-[3rem]"> Button to go to Dashboard</h1>
+      <h1 className="text-center text-[3rem]"> Fill in any other info</h1>
+      <h1 className="text-center text-[3rem]">Delete & Logout btn</h1>
     </>
   );
 }
