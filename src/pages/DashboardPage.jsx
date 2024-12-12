4dbacefee8d9220f5dace3fc10e1360c42f6007d
Mark Taratynov <mark.taratynov@gmail.com>
2024-12-12 16:19:20 +0100

    Removed some errors from mark branch

---
+++ src/pages/DashboardPage.jsx
@@ -0,0 +1,21 @@
+import { useUserContext } from "../context/UserContext";
+import Nav from "../layout/Nav";
+
+export default function DashboardPage({ setIsLoggedIn }) {
+  return (
+    <>
+      <Nav setIsLoggedIn={setIsLoggedIn} />
+      <h1 className="text-center text-[3rem]"> Dashboard/Home </h1>
+      <h1 className="text-center text-[2rem]">
+        User sees this page upon authentication (Logged in)
+      </h1>
+      <h1 className="text-center text-[3rem]"> Greet user. Hi (User) </h1>
+      <h1 className="text-center text-[3rem]">
+        Random Quote via API everytime user enters the Dashboard
+      </h1>
+      <h1 className="text-center text-[3rem]">
+        Overview showing user activity. Minimalistic.
+      </h1>
+    </>
+  );
+}
