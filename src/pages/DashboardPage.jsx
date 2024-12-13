ded0a89329e787b76a2888b0b0dc666199bb836f
Mark Taratynov <mark.taratynov@gmail.com>
2024-12-13 22:04:13 +0100

    Moved api request, and profile name into dashboard. It renders upon you refresh a webpage

--- src/pages/DashboardPage.jsx
+++ src/pages/DashboardPage.jsx
@@ -1,21 +1,63 @@
-import { useUserContext } from "../context/UserContext";
 import Nav from "../layout/Nav";
+import { useEffect, useState } from "react";
 
 export default function DashboardPage({ setIsLoggedIn }) {
+  const [quote, setQuote] = useState(null);
+  const [username, setUsername] = useState(null);
+  const capitalize = (str) => {
+    return str.charAt(0).toUpperCase() + str.slice(1);
+  };
+
+  useEffect(() => {
+    const fetchQuote = async () => {
+      try {
+        const response = await fetch("https://api.quotable.io/random");
+        const data = await response.json();
+        console.log(data);
+        setQuote(data);
+      } catch (error) {
+        console.error("Error during loading a quote:", error);
+      }
+    };
+    setTimeout(() => {
+      fetchQuote();
+    }, 500);
+  }, []);
+
+  useEffect(() => {
+    const currentUser = JSON.parse(localStorage.getItem("currentUser"));
+
+    if (currentUser) {
+      setUsername(currentUser.username);
+    } else {
+      console.log("No user logged in");
+    }
+  }, []);
   return (
     <>
       <Nav setIsLoggedIn={setIsLoggedIn} />
-      <h1 className="text-center text-[3rem]"> Dashboard/Home </h1>
-      <h1 className="text-center text-[2rem]">
-        User sees this page upon authentication (Logged in)
-      </h1>
-      <h1 className="text-center text-[3rem]"> Greet user. Hi (User) </h1>
-      <h1 className="text-center text-[3rem]">
-        Random Quote via API everytime user enters the Dashboard
-      </h1>
       <h1 className="text-center text-[3rem]">
         Overview showing user activity. Minimalistic.
       </h1>
+      {username ? (
+        <h1 className="text-4xl font-bold text-center my-5">{`Hello, ${capitalize(
+          username
+        )}`}</h1>
+      ) : (
+        <p>User is not defined</p>
+      )}
+      {quote ? (
+        <h1 className="text-xl text-center my-5">{`${quote.author} said: ${quote.content}`}</h1>
+      ) : (
+        <div className="flex items-center gap-2 w-full justify-center">
+          <img
+            className="w-8 animate-spin"
+            src="/src/assets/loading.png"
+            alt=""
+          />
+          <p className="text-center font-bold text-2xl">Loading...</p>
+        </div>
+      )}
     </>
   );
 }
--- src/pages/ProfilePage.jsx
+++ src/pages/ProfilePage.jsx
@@ -5,68 +5,15 @@ import { useState, useEffect } from "react";
 import { useUserContext } from "../context/UserContext";
 
 export default function ProfilePage({ setIsLoggedIn }) {
-  const { error, quote, getQuote } = useUserContext();
-  const [username, setUsername] = useState(null);
-
-  useEffect(() => {
-    setTimeout(() => {
-      getQuote();
-    }, 500);
-  }, []);
-
   const navigate = useNavigate();
 
   const { deleteAccount } = useAccount(navigate, setIsLoggedIn);
-
-  const capitalize = (str) => {
-    return str.charAt(0).toUpperCase() + str.slice(1);
-  };
-
-  useEffect(() => {
-    const currentUser = JSON.parse(localStorage.getItem("currentUser"));
-
-    if (currentUser) {
-      setUsername(currentUser.username);
-    } else {
-      console.log("No user logged in");
-    }
-  }, []);
 
   return (
     <>
       <Nav />
       <section className="flex flex-col gap-5 items-center h-screen">
-        <div className="flex flex-col ">
-          <h1 className="text-center text-5xl my-5">
-            Greetings,{" "}
-            <span className="">
-              {username ? capitalize(username) : "No user logged in"}
-            </span>
-          </h1>
-          {error ? (
-            <p>Error: {error}</p>
-          ) : (
-            <>
-              {Array.isArray(quote) && quote.length > 0 ? (
-                quote.map((item, i) => (
-                  <div key={i}>
-                    <blockquote className="italic font-bold text-center border border-slate-400 rounded-lg px-5 py-2 my-5">
-                      The quote of day: {item.quote}
-                    </blockquote>
-                  </div>
-                ))
-              ) : (
-                <div className="flex items-center gap-2 justify-center my-5">
-                  <img
-                    className="w-9 text-center animate-spin"
-                    src="/src/assets/loading.png"
-                  />
-                  <p>Loading..</p>
-                </div>
-              )}
-            </>
-          )}
-        </div>
+        <div className="flex flex-col "></div>
 
         <button
           className="px-5 py-1 text-xl border border-black text-white font-bold rounded-lg drop-shadow-md bg-sortify cursor-pointer hover:scale-105 hover:bg-slate-500 transition-all duration-200"
