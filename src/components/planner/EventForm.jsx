32f80186f59c837d2eba8502d53992b5d46e105e
Mark Taratynov <mark.taratynov@gmail.com>
2024-12-21 20:46:29 +0100

    Add animation to EventForm component for improved user experience

--- src/components/planner/EventForm.jsx
+++ src/components/planner/EventForm.jsx
@@ -4,6 +4,7 @@ import { useRef } from "react";
 import { BiHide } from "react-icons/bi";
 import { AiOutlinePlus } from "react-icons/ai";
 import { MdOutlineEmojiEvents } from "react-icons/md";
+import { AnimatePresence, motion } from "motion/react";
 export default function EventForm({
   handleForm,
   setShowForm,
@@ -28,73 +29,90 @@ export default function EventForm({
 
   return (
     <>
-      {user ? (
-        <form
-          onSubmit={(e) => handleSubmit(e, setShowForm, setShowAddEvent)}
-          className="event-form-inner-form"
+      <AnimatePresence>
+        <motion.form
+          initial={{ opacity: 0 }}
+          animate={{ opacity: 1, transition: { duration: 0.6 } }}
+          exit={{ x: 100 }}
+          className="all-task-inputs"
         >
-          <h2 className="add-new-event-headline">Add a New Event</h2>
-          <div className="flex items-center gap-2">
-            <MdOutlineEmojiEvents style={{ opacity: 0.5 }} size={40} />
-            <input
-              id="event-name"
-              className="event-form-input"
-              type="text"
-              placeholder="Enter event name"
-              value={name}
-              ref={nameRef}
-              onChange={(e) => setName(e.target.value)}
-            />
-          </div>
-          <div className="flex gap-2 max-sm:flex-col max-sm:items-center">
-            <div className="w-full max-sm:flex max-sm:items-center max-sm:justify-between">
-              <label htmlFor="start-date" className="event-form-label">
-                Start
-              </label>
-              <input
-                id="start-date"
-                className="event-form-input"
-                type="datetime-local"
-                value={start}
-                ref={startRef}
-                onChange={(e) => setStart(e.target.value)}
-              />
-            </div>
-            <div className="w-full max-sm:flex max-sm:items-center max-sm:justify-between">
-              <label htmlFor="end-date" className="event-form-label">
-                End
-              </label>
-              <input
-                id="end-date"
-                className="event-form-input"
-                type="datetime-local"
-                value={end}
-                min={start || ""}
-                ref={endRef}
-                onChange={(e) => setEnd(e.target.value)}
-              />
-            </div>
-          </div>
-          <button type="submit" className="addtaskbtn justify-center">
-            <AiOutlinePlus size={22} />
-          </button>
-          <button type="button" onClick={handleForm} className="hidetaskbtn">
-            <BiHide size={25} color="white" />
-          </button>
-        </form>
-      ) : (
-        <p className="text-gray-600">
-          Please <span className="font-semibold text-blue-600">log in</span> to
-          add events.
-        </p>
-      )}
-      {error ? (
-        <h1 className={`text-red-500 text-center mt-5 ${shake ? "shake" : ""}`}>
-          {error}
-        </h1>
-      ) : (
-        <p>{""}</p>
-      )}
+          {user ? (
+            <form
+              onSubmit={(e) => handleSubmit(e, setShowForm, setShowAddEvent)}
+              className="event-form-inner-form"
+            >
+              <h2 className="add-new-event-headline">Add a New Event</h2>
+              <div className="flex items-center gap-2">
+                <MdOutlineEmojiEvents style={{ opacity: 0.5 }} size={40} />
+                <input
+                  id="event-name"
+                  className="event-form-input"
+                  type="text"
+                  placeholder="Enter event name"
+                  value={name}
+                  ref={nameRef}
+                  onChange={(e) => setName(e.target.value)}
+                />
+              </div>
+              <div className="flex gap-2 max-sm:flex-col max-sm:items-center">
+                <div className="w-full max-sm:flex max-sm:items-center max-sm:justify-between">
+                  <label htmlFor="start-date" className="event-form-label">
+                    Start
+                  </label>
+                  <input
+                    id="start-date"
+                    className="event-form-input"
+                    type="datetime-local"
+                    value={start}
+                    ref={startRef}
+                    onChange={(e) => setStart(e.target.value)}
+                  />
+                </div>
+                <div className="w-full max-sm:flex max-sm:items-center max-sm:justify-between">
+                  <label htmlFor="end-date" className="event-form-label">
+                    End
+                  </label>
+                  <input
+                    id="end-date"
+                    className="event-form-input"
+                    type="datetime-local"
+                    value={end}
+                    min={start || ""}
+                    ref={endRef}
+                    onChange={(e) => setEnd(e.target.value)}
+                  />
+                </div>
+              </div>
+              <button type="submit" className="addtaskbtn justify-center">
+                <AiOutlinePlus size={22} />
+              </button>
+              <button
+                type="button"
+                onClick={handleForm}
+                className="hidetaskbtn"
+              >
+                <BiHide size={25} color="white" />
+              </button>
+            </form>
+          ) : (
+            <p className="text-gray-600">
+              Please <span className="font-semibold text-blue-600">log in</span>{" "}
+              to add events.
+            </p>
+          )}
+          {error ? (
+            <h1
+              className={`text-red-500 text-center mt-5 ${
+                shake ? "shake" : ""
+              }`}
+            >
+              {error}
+            </h1>
+          ) : (
+            <p>{""}</p>
+          )}
+        </motion.form>
+      </AnimatePresence>
     </>
   );
 }
